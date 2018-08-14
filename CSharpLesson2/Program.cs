59f665a7b0f8c18a1664efb53d7ebbc9aba074d2
Jonathan Wilson <jon.wilsond@gmail.com>
2018-08-14 10:30:31 -0400

    updated program.cs

--- CSharpLesson2/Program.cs
+++ CSharpLesson2/Program.cs
@@ -6,9 +6,23 @@ using System.Threading.Tasks;
 
 namespace CSharpLesson2 {
 	class Program {
-		static void Main(string[] args) {
 
-			//test
+		static int Add(int nbr,int inc) {
+			return nbr + inc;
+		}
+
+		static int Add(int nbr) {
+			return Add(nbr, 1);
+		}
+		static void Main(string[] args) {
+			int nbr = 12;
+			int inc = 3;
+			int result = Add(nbr);
+			Console.WriteLine("{0} Add1 is {1}",nbr, result);
+
+			result = Add(nbr, inc);
+			Console.WriteLine("{0} Add1 is {1}", nbr, result);
+
 		}
 	}
 }
